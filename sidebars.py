3d2e9e245bd079df7d926a7937206125ff856d21
rineheaj <joshtrineheart@gmail.com>
2024-10-07 17:08:11 -0500

    Automated commit on 2024-10-07 17:08:11 Changed files: sidebars.py

--- sidebars.py
+++ sidebars.py
@@ -4,18 +4,18 @@ import os
 from data_utils import save_json_data, create_new_job
 from github import commit_to_github
 
-
 def check_used_mem():
     process = psutil.Process(os.getpid())
     mem_info = process.memory_info()
-    used_mem = mem_info.rss / (1024**2)
+    used_mem = mem_info.rss / (1024 **2)
     return used_mem
 
-
 def add_mem_button():
+    
     st.markdown(
-        """
-        <style>.sidebar .stButton button{
+        '''
+        <style>
+        div.stButton > button:first-child {
             background-color: orange;
             color: white;
             border: none;
@@ -29,47 +29,40 @@ def add_mem_button():
             border-radius: 4px;
         }
         </style>
-        """,
-        unsafe_allow_html=True,
+        ''', 
+        unsafe_allow_html=True
     )
-
-    if st.sidebar.button("Check Memory Usage", key="mem_button"):
+    if st.sidebar.button('Check Memory Usage', key='mem_button'):
         used_memory = check_used_mem()
-        st.sidebar.info(f"Memory Usage: {used_memory:.2f} MB")
-
+        st.sidebar.info(f'Memory Usage: {used_memory:.2f} MB')
 
 def refresh_page():
     if st.sidebar.button("Refresh Page"):
         st.session_state.update()
 
-
 def add_job():
     st.sidebar.subheader("➕ Add New Job Application")
     company = st.sidebar.text_input("Company")
     position = st.sidebar.text_input("Position")
-    status = st.sidebar.selectbox(
-        "Status", ["Applied", "Interviewing", "Offer", "Rejected"]
-    )
+    status = st.sidebar.selectbox("Status", ["Applied", "Interviewing", "Offer", "Rejected"])
     applied_date = st.sidebar.date_input("Applied Date")
 
     if st.sidebar.button("Add Job"):
         new_job = create_new_job(
-            app_date=applied_date, co=company, pos=position, status=status
+            app_date=applied_date,
+            co=company,
+            pos=position,
+            status=status
         )
         st.session_state["job_data"].append(new_job)
         save_json_data(st.session_state["job_data"])
         commit_to_github(st.session_state["job_data"])
         st.success(f"Job added: {company} - {position} - {status}")
 
-
 def update_job_status(df):
     st.sidebar.subheader("🔄 Update Job Status")
-    job_to_update = st.sidebar.selectbox(
-        "Select Job to Update", df["Position"].unique()
-    )
-    new_status = st.sidebar.selectbox(
-        "New Status", ["Applied", "Interviewing", "Offer", "Rejected"]
-    )
+    job_to_update = st.sidebar.selectbox("Select Job to Update", df["Position"].unique())
+    new_status = st.sidebar.selectbox("New Status", ["Applied", "Interviewing", "Offer", "Rejected"])
 
     if st.sidebar.button("Update Status"):
         index_to_update = df[df["Position"] == job_to_update].index[0]
@@ -77,7 +70,6 @@ def update_job_status(df):
         save_json_data(st.session_state["job_data"])
         commit_to_github(st.session_state["job_data"])
         st.success(f"Status updated for {job_to_update} to {new_status}")
-
 
 def delete_job(df):
     st.sidebar.subheader("🗑️ Delete a Job Listing")
@@ -90,10 +82,10 @@ def delete_job(df):
         commit_to_github(st.session_state["job_data"])
         st.success(f'Job "{job_to_del}" deleted')
 
-
 def sidebar(df):
     refresh_page()
     add_mem_button()
     add_job()
     update_job_status(df)
     delete_job(df)
+    
