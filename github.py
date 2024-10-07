f1c33a3cfac1fbf6b2c09b19d9377c2d801cd2a5
rineheaj <joshtrineheart@gmail.com>
2024-10-07 03:57:24 -0500

    Automated commit

--- github.py
+++ github.py
@@ -14,32 +14,39 @@ def commit_to_github(data):
         'Content-Type': 'application/json'
     }
 
-    response = requests.get(url=url, headers=headers)
-    response_json = response.json()
-    
-    
-    if 'sha' in response_json:
-        sha = response_json['sha']
-    else:
-        st.error('SHA key not found in the response.')
-        return
-    
-    # B64ENCODED
-    content = base64.b64encode(
-        json.dumps(data).encode('utf-8')
-    ).decode('utf-8')
+    try:
+        response = requests.get(url=url, headers=headers)
+        response.raise_for_status()
+        response_json = response.json()
 
-    commit_data = {
-        'message': f'Update job data {datetime.now().isoformat()}',
-        'content': content,
-        'sha': sha
-    }
-    response = requests.put(url, 
-                            headers=headers, 
-                            data=json.dumps(commit_data)
-    )
-    if response.status_code == 200:
-        st.success('Changes committed to GitHub successfully')
-    else:
-        st.error('Failed to commit changes to GitHub.')
+        if 'sha' in response_json:
+            sha = response_json['sha']
+        else:
+            st.error('SHA key not found in the response.')
+            return
+
+        content = base64.b64encode(
+            json.dumps(data).encode('utf-8')
+        ).decode('utf-8')
+
+        commit_data = {
+            'message': f'Update job data {datetime.now().isoformat()}',
+            'content': content,
+            'sha': sha
+        }
+
+        response = requests.put(url, 
+                                headers=headers, 
+                                data=json.dumps(commit_data))
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            st.success('Changes committed to GitHub successfully')
+        else:
+            st.error('Failed to commit changes to GitHub.')
+
+    except requests.exceptions.RequestException as e:
+        st.error(f'An error occurred: {e}')
+    finally:
+        st.info('Attempt to commit changes to GitHub has completed.')
     
--- test_jobs_board.py
+++ test_jobs_board.py
@@ -5,30 +5,31 @@ import json
 from github import commit_to_github
 from charts import create_charts, display_charts
 
+
 ##LOAD DATA
 def load_json_data():
     try:
-        with open('job_data.json', 'r') as f:
+        with open("job_data.json", "r") as f:
             return json.load(f)
     except FileNotFoundError:
         return []
 
-#SAVE DATA
+
+# SAVE DATA
 def save_json_data(data):
-    with open('job_data.json', 'w') as f:
+    with open("job_data.json", "w") as f:
         json.dump(data, f, indent=4)
 
 
 # CREATE DATA STRUCTURE
-if 'job_data' not in st.session_state:
-    st.session_state['job_data'] = load_json_data()
+if "job_data" not in st.session_state:
+    st.session_state["job_data"] = load_json_data()
+
 
 def create_job_table():
-    """
-    Create a DataFrame displaying job application details.
-    """
-    df = pd.DataFrame(st.session_state['job_data'])
+    df = pd.DataFrame(st.session_state["job_data"])
     return df
+
 
 # SET TITLE
 def set_title():
@@ -40,63 +41,66 @@ def set_title():
         
         <h1 class="title">🎯 Job Application Tracker</h1>
         """,
-        unsafe_allow_html=True
+        unsafe_allow_html=True,
     )
     return title
+
 
 main_title = set_title()
 
 title2 = st.markdown(
-    '''
-    <style> .title2 {text-align: center; padding: 40px;}
+    """
+    <style> .title2 {text-align: center; padding: 25px;}
     </style>
 
     <h1 class="title2">📋 Job Applications</h1>
-    ''',
-    unsafe_allow_html=True
+    """,
+    unsafe_allow_html=True,
 )
 
-# CREATE DF 
+# CREATE DF
 df = create_job_table()
+
 
 # COLORS FOR DF
 def status_color(val):
-    """Function to return the background color based on job status."""
-    if val == 'Offer':
-        return 'background-color: green;'
-    elif val == 'Rejected':
-        return 'background-color: red;'
-    elif val == 'Interviewing':
-        return 'background-color: coral;'
-    elif val == 'Applied':
-        return 'background-color: blue;'
+    if val == "Offer":
+        return "background-color: green;"
+    elif val == "Rejected":
+        return "background-color: red;"
+    elif val == "Interviewing":
+        return "background-color: coral;"
+    elif val == "Applied":
+        return "background-color: blue;"
     else:
-        return ''
+        return ""
+
 
 # DATAFRAME STYLES
 styled_df = (
-    df.style
-    .map(lambda x: 'background-color: gray;', subset=['Company'])
-    .map(lambda x: 'background-color: darkcyan;', subset=['Position'])
-    .map(status_color, subset=['Status'])
+    df.style.map(lambda x: "background-color: gray;", subset=["Company"])
+    .map(lambda x: "background-color: darkcyan;", subset=["Position"])
+    .map(status_color, subset=["Status"])
     # .map(lambda x: 'background-color: purple;', subset=['Website URL'])
-    .map(lambda x: 'background-color: darkgreen;', subset=['Applied Date'])
+    .map(lambda x: "background-color: darkgreen;", subset=["Applied Date"])
 )
 
-#AUTO RESIZE DF
+# AUTO RESIZE DF
 st.dataframe(styled_df, use_container_width=True)
 
 # REFRESH PAGE BUTTON
-if st.sidebar.button('Refresh Page'):
+if st.sidebar.button("Refresh Page"):
     st.session_state.update()
 
 # ADD NEW JOB TO TABLE
 st.sidebar.subheader("➕ Add New Job Application")
 company = st.sidebar.text_input("Company")
 position = st.sidebar.text_input("Position")
-status = st.sidebar.selectbox("Status", ['Applied', 'Interviewing', 'Offer', 'Rejected'])
+status = st.sidebar.selectbox(
+    "Status", ["Applied", "Interviewing", "Offer", "Rejected"]
+)
 # website_url = st.sidebar.text_input('Website URL')
-applied_date = st.sidebar.date_input('Applied Date')
+applied_date = st.sidebar.date_input("Applied Date")
 
 # ADD JOB BUTTON
 if st.sidebar.button("Add Job"):
@@ -104,41 +108,38 @@ if st.sidebar.button("Add Job"):
         "Applied Date": str(applied_date),
         "Company": company,
         "Position": position,
-        "Status": status
+        "Status": status,
     }
     # st.session_state['job_data']['Website URL'].append(website_url)
-    st.session_state['job_data'].append(new_job)
-    save_json_data(st.session_state['job_data'])
-    commit_to_github(st.session_state['job_data'])
-    st.success(f'Job added: {company} - {position} - {status}')
+    st.session_state["job_data"].append(new_job)
+    save_json_data(st.session_state["job_data"])
+    commit_to_github(st.session_state["job_data"])
+    st.success(f"Job added: {company} - {position} - {status}")
 
 
-
-#UPDATE JOB STATUS
-st.sidebar.subheader('🔄 Update Job Status')
-job_to_update = st.sidebar.selectbox('Select Job to Update', df['Position'].unique())
-new_status = st.sidebar.selectbox('New Status', [
-    'Applied', 'Interviewing', 'Offer', 'Rejected'
-])
-
-if st.sidebar.button("Update Status"):
-    index_to_update = df[df['Position'] == job_to_update].index[0]
-    st.session_state['job_data'][index_to_update]['Status'] = new_status
-    save_json_data(st.session_state['job_data'])
-    commit_to_github(st.session_state['job_data'])
-    st.success(f'Status updated for {job_to_update} to {new_status}')
-
-#DELETE JOB LISTING
-st.sidebar.subheader('🗑️ Delete a Job Listing')
-job_to_del = st.sidebar.selectbox(
-    'Select Job to Delete', df['Position'].unique()
+# UPDATE JOB STATUS
+st.sidebar.subheader("🔄 Update Job Status")
+job_to_update = st.sidebar.selectbox("Select Job to Update", df["Position"].unique())
+new_status = st.sidebar.selectbox(
+    "New Status", ["Applied", "Interviewing", "Offer", "Rejected"]
 )
 
-if st.sidebar.button('Delete a Job'):
-    i_to_del = df[df['Position'] == job_to_del].index[0]
-    st.session_state['job_data'].pop(i_to_del)
-    save_json_data(st.session_state['job_data'])
-    commit_to_github(st.session_state['job_data'])
+if st.sidebar.button("Update Status"):
+    index_to_update = df[df["Position"] == job_to_update].index[0]
+    st.session_state["job_data"][index_to_update]["Status"] = new_status
+    save_json_data(st.session_state["job_data"])
+    commit_to_github(st.session_state["job_data"])
+    st.success(f"Status updated for {job_to_update} to {new_status}")
+
+# DELETE JOB LISTING
+st.sidebar.subheader("🗑️ Delete a Job Listing")
+job_to_del = st.sidebar.selectbox("Select Job to Delete", df["Position"].unique())
+
+if st.sidebar.button("Delete a Job"):
+    i_to_del = df[df["Position"] == job_to_del].index[0]
+    st.session_state["job_data"].pop(i_to_del)
+    save_json_data(st.session_state["job_data"])
+    commit_to_github(st.session_state["job_data"])
     st.success(f'Job "{job_to_del}" deleted')
 
 
@@ -146,20 +147,7 @@ pie_chart, bar_chart1, bar_chart2 = create_charts(df=df)
 display_charts(pie_chart, bar_chart1, bar_chart2)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-###DONT USE FOR NOW
+#               ##DONT USE FOR NOW##
 # #TEST TIMELINE CHART
 # import plotly.express as px
 
