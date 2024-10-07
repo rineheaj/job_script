acaa46122e5a6a801b9a9bdfe7118262dc938228
rineheaj <joshtrineheart@gmail.com>
2024-10-07 05:45:15 -0500

    Automated commit on 2024-10-07 05:45:15 Changed files: sidebars.py

--- sidebars.py
+++ sidebars.py
@@ -3,25 +3,17 @@ import pandas as pd
 from data_utils import save_json_data, create_new_job
 from github import commit_to_github
 
-
-
-
-def sidebar(df):
-    # REFRESH PAGE BUTTON
+def refresh_page():
     if st.sidebar.button("Refresh Page"):
         st.session_state.update()
 
-    # ADD NEW JOB TO TABLE
+def add_job():
     st.sidebar.subheader("➕ Add New Job Application")
     company = st.sidebar.text_input("Company")
     position = st.sidebar.text_input("Position")
-    status = st.sidebar.selectbox(
-        "Status", ["Applied", "Interviewing", "Offer", "Rejected"]
-    )
-    # website_url = st.sidebar.text_input('Website URL')
+    status = st.sidebar.selectbox("Status", ["Applied", "Interviewing", "Offer", "Rejected"])
     applied_date = st.sidebar.date_input("Applied Date")
 
-    # ADD JOB BUTTON
     if st.sidebar.button("Add Job"):
         new_job = create_new_job(
             app_date=applied_date,
@@ -29,19 +21,15 @@ def sidebar(df):
             pos=position,
             status=status
         )
-        # st.session_state['job_data']['Website URL'].append(website_url)
         st.session_state["job_data"].append(new_job)
         save_json_data(st.session_state["job_data"])
         commit_to_github(st.session_state["job_data"])
         st.success(f"Job added: {company} - {position} - {status}")
 
-
-    # UPDATE JOB STATUS
+def update_job_status(df):
     st.sidebar.subheader("🔄 Update Job Status")
     job_to_update = st.sidebar.selectbox("Select Job to Update", df["Position"].unique())
-    new_status = st.sidebar.selectbox(
-        "New Status", ["Applied", "Interviewing", "Offer", "Rejected"]
-    )
+    new_status = st.sidebar.selectbox("New Status", ["Applied", "Interviewing", "Offer", "Rejected"])
 
     if st.sidebar.button("Update Status"):
         index_to_update = df[df["Position"] == job_to_update].index[0]
@@ -50,7 +38,7 @@ def sidebar(df):
         commit_to_github(st.session_state["job_data"])
         st.success(f"Status updated for {job_to_update} to {new_status}")
 
-    # DELETE JOB LISTING
+def delete_job(df):
     st.sidebar.subheader("🗑️ Delete a Job Listing")
     job_to_del = st.sidebar.selectbox("Select Job to Delete", df["Position"].unique())
 
@@ -60,3 +48,9 @@ def sidebar(df):
         save_json_data(st.session_state["job_data"])
         commit_to_github(st.session_state["job_data"])
         st.success(f'Job "{job_to_del}" deleted')
+
+def sidebar(df):
+    refresh_page()
+    add_job()
+    update_job_status(df)
+    delete_job(df)
