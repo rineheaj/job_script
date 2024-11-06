1c54f36ccfd1760415ade4297ec46fb914a8f939
rineheaj <joshtrineheart@gmail.com>
2024-11-05 23:07:01 -0600

    Automated commit on 11-05-2024 23:07:01 Changed files: second_page.py

--- second_page.py
+++ second_page.py
@@ -12,7 +12,7 @@ def show_second_page():
         
     )
     st.markdown(
-        "<h1>This page provides various statistics about your job applications.</h1>",
+        "<h2>This page provides various statistics about your job applications.</h2>",
         unsafe_allow_html=True
             
     )
